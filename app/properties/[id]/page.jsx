632c07cb1c81edd4a0bbe8d5a1e04847957b5334
EkremL <ekremlale1907@gmail.com>
2025-01-28 18:12:12 +0300

    before mongodb

---
+++ app/properties/[id]/page.jsx
@@ -0,0 +1,10 @@
+const PropertyPage = ({ params }) => {
+  return (
+    <div>
+      Property Page {params.id}
+      {/* buradaki id dosya isminde belirttiğimiz için */}
+    </div>
+  );
+};
+
+export default PropertyPage;
